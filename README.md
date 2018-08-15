ebaf66ceb8484ce8aeb6de5f1fc08c736b5bcc31
geoolson <geoolson@pdx.edu>
2018-08-15 14:11:09 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# KinectKHR1
+KinectKHR1 translates a person's movement into angles to control KHR1 with.
---
+++ ServoPosition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Kinect;
+using System.Windows.Media.Media3D;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    internal class ServoPosition
+    {
+        internal Double ZRotation(Joint A, Joint Origin, Joint C)
+        {  
+            Vector a = new Vector(A.Position.X - Origin.Position.X, A.Position.Y - Origin.Position.Y);
+            Vector c = new Vector(C.Position.X - Origin.Position.X, C.Position.Y - Origin.Position.Y);
+            return Vector.AngleBetween(a, c);
+        }
+
+        internal Double YRotation(Joint A, Joint Origin, Joint C)
+        {
+            Vector3D a = new Vector3D(A.Position.X - Origin.Position.X,
+                                      A.Position.Y - Origin.Position.Y,
+                                      A.Position.Z - Origin.Position.Z);
+            Vector3D c = a;
+            c.X = 0;
+            return Vector3D.AngleBetween(a, c);
+        }
+
+        internal Double XRotation(Joint A, Joint Origin, Joint C)
+        {
+            Vector a = new Vector(A.Position.Y - Origin.Position.Y, A.Position.Z - Origin.Position.Z);
+            Vector c = new Vector(C.Position.Y - Origin.Position.Y, C.Position.Z - Origin.Position.Z);
+            return Vector.AngleBetween(a, c);
+        }
+
+        internal Double Angle3D(Joint A, Joint Origin, Joint C)
+        {
+            Vector3D a = new Vector3D(A.Position.X - Origin.Position.X,
+                                      A.Position.Y - Origin.Position.Y,
+                                      A.Position.Z - Origin.Position.Z);
+            Vector3D c = new Vector3D(C.Position.X - Origin.Position.X,
+                                      C.Position.Y - Origin.Position.Y,
+                                      C.Position.Z - Origin.Position.Z);
+            return Vector3D.AngleBetween(a, c);
+        }
+    }
+}
